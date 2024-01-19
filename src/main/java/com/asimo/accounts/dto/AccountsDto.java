2cd20ee53447a6dfb324f5a8a437511a40db569d
Asim <asimtimsina@my.unt.edu>
2024-01-18 21:57:22 -0600

    14. Introduction to DTO (Data Transfer Object) pattern 15. Creating DTOs inside accounts microservice

---
+++ src/main/java/com/asimo/accounts/dto/AccountsDto.java
@@ -0,0 +1,19 @@
+package com.asimo.accounts.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class AccountsDto {
+
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+
+
+}
