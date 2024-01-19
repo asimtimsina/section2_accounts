7642bc19e51d76c51ceb747d740372bd86e33cff
Asim <asimtimsina@my.unt.edu>
2024-01-18 22:57:00 -0600

    17. CREATE API inside accounts microservice - Part 2

--- src/main/java/com/asimo/accounts/service/AccountsService.java
+++ src/main/java/com/asimo/accounts/service/AccountsService.java
@@ -1,11 +1,19 @@
 package com.asimo.accounts.service;
 
-import com.asimo.accounts.dto.AccountsDto;
+import com.asimo.accounts.constants.AccountConstants;
 import com.asimo.accounts.dto.CustomerDto;
+import com.asimo.accounts.entity.Accounts;
+import com.asimo.accounts.entity.Customer;
+import com.asimo.accounts.exception.CustomerAlreadyExistsException;
+import com.asimo.accounts.mapper.CustomerMapper;
 import com.asimo.accounts.repository.AccountsRepo;
 import com.asimo.accounts.repository.CustomerRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -15,8 +23,47 @@ public class AccountsService implements  IAccountsService{
     private CustomerRepo customerRepo;
 
 
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
 
+        Optional<Customer> customerExists = customerRepo.findByMobileNumber(customer.getMobileNumber());
+        if(customerExists.isPresent()){
+            throw new CustomerAlreadyExistsException("Customer already exists with given mobileNumber"
+                    + customer.getMobileNumber());
+        }
+
+        customer.setCreatedBy("Tester");
+        customer.setCreatedAt(LocalDateTime.now());
+
+        customerRepo.save(customer);
+        accountsRepo.save(createNewAccount(customer));
     }
+
+    /**
+     *
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    @Override
+    public Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setCreatedBy("Tester");
+        newAccount.setCreatedAt(LocalDateTime.now());
+
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountConstants.ADDRESS);
+        return newAccount;
+    }
+
+
 }
--- src/main/java/com/asimo/accounts/service/IAccountsService.java
+++ src/main/java/com/asimo/accounts/service/IAccountsService.java
@@ -1,6 +1,8 @@
 package com.asimo.accounts.service;
 
 import com.asimo.accounts.dto.CustomerDto;
+import com.asimo.accounts.entity.Accounts;
+import com.asimo.accounts.entity.Customer;
 
 public interface IAccountsService {
 
@@ -9,4 +11,11 @@ public interface IAccountsService {
      * @param customerDto - CustomerDto Object
      */
     void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    Accounts createNewAccount(Customer customer);
 }
